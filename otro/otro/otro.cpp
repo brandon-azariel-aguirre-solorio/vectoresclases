0f5300b4b109a0d339683eb49845ee3eeafc1b30
brandon azariel aguirre solorio <baguirre1@ucol.mx>
2019-11-13 15:34:37 -0600

    final

--- otro/otro/otro.cpp
+++ otro/otro/otro.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main()
 {
 	int numeros[10];
-	int i, num, suma, resta, multiplicacion, divicion;
+	int i, num, suma, resta, multiplicacion;
+	int divicion;
 	suma = 0;
 	resta = 0;
-	multiplicacion = 1;
 	divicion = 1;
+	multiplicacion = 1;
 	for (i = 0; i <= 9; i++)
 	{
 		cout << "Introduce un numero: ";
@@ -16,7 +17,7 @@ int main()
 		suma = suma + numeros[i];
 		resta = resta - numeros[i];
 		multiplicacion = multiplicacion * numeros[i];
-		divicion = divicion / numeros[i];
+		divicion = numeros[i] / divicion;
 	}
 	cout << "La suma es de: " << suma << endl;
 	cout << "La resta es de: " << resta << endl;
