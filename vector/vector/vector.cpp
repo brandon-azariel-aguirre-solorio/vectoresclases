49740825f9f1a8654a5e7c03d5c2f6f7abfdda95
Saulo <sau890p@gmail.com>
2019-11-13 11:20:51 -0600

    final

--- vector/vector/vector.cpp
+++ vector/vector/vector.cpp
@@ -3,21 +3,21 @@
 using namespace std;
 int main()
 {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	int x, contador, i, tabla[100];
+	i = 0;
+	for (x = 1; x <= 100; x++)
+	{
+		contador = 0;
+		if (x % 2 == 0)
+		{
+			tabla[i] = x;
+			i++;
+		}
+	}
+	for (x=0;x<i;x++)
+	{
+		cout << tabla[x] << endl;
+	}
+	system("pause");
+	return 0;
 }
